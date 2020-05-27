ec85b8d36373a9f4246d8e882b3fa185a5b6fd08
mostafa07 <mostafamahmoud07uv@gmail.com>
2020-05-27 18:50:58 +0200

    feat: added okhttp logging interceptor

--- app/src/main/java/com/example/alexr/ideamanager/services/ServiceBuilder.java
+++ app/src/main/java/com/example/alexr/ideamanager/services/ServiceBuilder.java
@@ -1,5 +1,7 @@
 package com.example.alexr.ideamanager.services;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,8 +9,16 @@ public class ServiceBuilder {
 
     private static final String BASE_URL = "http://10.0.2.2:9000/";
 
+    private static HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    private static OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor);
+
     private static final Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create());
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClientBuilder.build());
+
     private static Retrofit retrofit = retrofitBuilder.build();
 
     public static <S> S buildService(Class<S> serviceType) {
