09b90506c195fc3cb903e5c01de5c96a196acefc
mostafa07 <mostafamahmoud07uv@gmail.com>
2020-05-28 18:43:21 +0200

    feat: added custom http headers okhttp interceptors

--- app/src/main/java/com/example/alexr/ideamanager/services/builder/ServiceBuilder.java
+++ app/src/main/java/com/example/alexr/ideamanager/services/builder/ServiceBuilder.java
@@ -1,6 +1,16 @@
 package com.example.alexr.ideamanager.services.builder;
 
+import android.os.Build;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -9,10 +19,24 @@ public class ServiceBuilder {
 
     private static final String BASE_URL = "http://10.0.2.2:9000/";
 
+    private static Interceptor customHttpHeadersInterceptor = new Interceptor() {
+        @NotNull
+        @Override
+        public Response intercept(@NotNull Chain chain) throws IOException {
+            Request request = chain.request();
+            request = request.newBuilder()
+                    .addHeader("Accept-Language", Locale.getDefault().getLanguage())
+                    .addHeader("x-device-type", Build.DEVICE)
+                    .build();
+
+            return chain.proceed(request);
+        }
+    };
     private static HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor()
             .setLevel(HttpLoggingInterceptor.Level.BODY);
 
     private static OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder()
+            .addInterceptor(customHttpHeadersInterceptor)
             .addInterceptor(loggingInterceptor);
 
     private static final Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl(BASE_URL)
